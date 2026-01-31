b4d1228d5fa476e12195bdbc06309e04220dd0d1
Nominom <7089998+Nominom@users.noreply.github.com>
2026-01-31 22:33:16 +0200

    Better cam movement

--- Assets/Scripts/CameraMeower.cs
+++ Assets/Scripts/CameraMeower.cs
@@ -1,3 +1,4 @@
+using System.Collections;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
@@ -5,9 +6,9 @@ public class CameraMeower : MonoBehaviour
 {
     [Tooltip("The screen x coordinate from the screen side where we rotate.")]
     public int CameraTurnXMargin = 50;
-    public float CameraMoveTime = 1f;
     public AnimationCurve CameraMoveCurve;
 
+    public float lerpDuration = 0.5f;
     public Transform cameraPositionLeft;
     public Transform cameraPositionRight;
 
@@ -17,30 +18,57 @@ public class CameraMeower : MonoBehaviour
         Right,
     }
     public CameraPositions CameraPosition;
-    
+
+    private Coroutine lerpRoutine;
     
 
     void Update()
     {
         if (Mouse.current.position.value.x < CameraTurnXMargin)
         {
-            CameraPosition = CameraPositions.Left;
+            if (lerpRoutine == null && CameraPosition != CameraPositions.Left)
+            {
+                CameraPosition = CameraPositions.Left;
+                lerpRoutine = StartCoroutine(Lerpa(cameraPositionLeft));
+            }
         }
         else if (Mouse.current.position.value.x >= Screen.width - CameraTurnXMargin)
         {
-            CameraPosition = CameraPositions.Right;
-        }
-
-        if (cameraPositionLeft && CameraPosition == CameraPositions.Left)
-        {
-            transform.position = cameraPositionLeft.position;
-            transform.rotation = cameraPositionLeft.rotation;
-        }
-
-        if (cameraPositionRight && CameraPosition == CameraPositions.Right)
-        {
-            transform.position = cameraPositionRight.position;
-            transform.rotation = cameraPositionRight.rotation;
+            if (lerpRoutine == null && CameraPosition != CameraPositions.Right)
+            {
+                CameraPosition = CameraPositions.Right;
+                lerpRoutine = StartCoroutine(Lerpa(cameraPositionRight));
+            }
         }
     }
+
+    IEnumerator Lerpa(Transform target)
+    {
+        Debug.Log("Panning to " + target.name, gameObject);
+        
+        if (target == null)
+            yield break;
+
+        Vector3 startPosition = transform.position;
+        Quaternion startRotation = transform.rotation;
+        Vector3 endPosition = target.position;
+        Quaternion endRotation = target.rotation;
+
+        float t = 0;
+
+        while (t < lerpDuration)
+        {
+            t += Time.deltaTime;
+            
+            float i = CameraMoveCurve.Evaluate(t / lerpDuration);
+            transform.position = Vector3.Lerp(startPosition, endPosition, i);
+            transform.rotation = Quaternion.Lerp(startRotation, endRotation, i);
+            
+            yield return null;
+        }
+        
+        lerpRoutine = null;
+    }
+    
+    
 }

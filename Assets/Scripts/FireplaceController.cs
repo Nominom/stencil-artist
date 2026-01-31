3c8893dc02469dfeb5c35bc7fca6c65824250fc8
Nominom <7089998+Nominom@users.noreply.github.com>
2026-01-31 15:36:43 +0200

    aaaaaaaaaaaa

--- Assets/Scripts/FireplaceController.cs
+++ Assets/Scripts/FireplaceController.cs
@@ -2,13 +2,10 @@ using UnityEngine;
 
 public class FireplaceController : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
+    public AnimationCurve flickerCurve;
+    public float intensity;
+    
 
-    // Update is called once per frame
     void Update()
     {
         

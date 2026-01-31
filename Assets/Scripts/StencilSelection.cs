f2fa78eb109f3c00e8fd4c2b45fd57ee5fddc462
NinjaChaan <16841526+NinjaChaan@users.noreply.github.com>
2026-01-31 22:08:52 +0200

    Stencil selection

---
+++ Assets/Scripts/StencilSelection.cs
@@ -0,0 +1,45 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StencilSelection : MonoBehaviour
+{
+    StencilScobj[] scobjs;
+    
+    public GameObject stencilPrefab;
+    
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        scobjs = Resources.LoadAll<StencilScobj>("Stencils");
+        LoadNewSelection();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void LoadNewSelection()
+    {
+        List<StencilScobj> stencils = new List<StencilScobj>();
+        List<int> picked = new List<int>();
+        
+        for (int i = 0; i < 3; i++)
+        {
+            int index = Random.Range(0, scobjs.Length);
+            while(picked.Contains(index))
+            {
+                index = Random.Range(0, scobjs.Length);
+            }
+            picked.Add(index);
+            var selected = scobjs[index];
+            Debug.Log(selected.name);
+            stencils.Add(selected);
+            var s = Instantiate(stencilPrefab, new Vector3(transform.position.x, transform.position.y + (i * 0.2f)-0.1f, transform.position.z+0.5f), transform.rotation);
+            var stencil = s.GetComponent<StencilScript>();
+            stencil.stencil = selected;
+            stencil.UpdateStencil();
+        }
+    }
+}

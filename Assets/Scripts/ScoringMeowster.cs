f64e9c9ebdc19080c8a8c646eeec5ee50bfddb9e
Jimi Jokela <jimi.jokela@live.fi>
2026-01-31 19:00:48 +0200

    Fixy fix

--- Assets/Scripts/ScoringMeowster.cs
+++ Assets/Scripts/ScoringMeowster.cs
@@ -7,23 +7,29 @@ public class ScoringMeowster : MonoBehaviour
     public int PlayerScore = 0;
     public int BaseScorePerStencil = 10;
 
-    void OnStencilPainted(StencilScobj stencil)
+    void OnStencilPainted(StencilObj stencil)
     {
         // Score
-        foreach(ScoringRule rule in stencil.scoringRules)
+        foreach(ScoringRule rule in stencil.data.scoringRules)
         {
             if (!string.IsNullOrEmpty(rule.AltTargetName))
             {
-
+                PlayerScore += GetNearbyStencilsByName(rule.AltTargetName, stencil).Count * rule.RewardScore;
+            }
+            else
+            {
+                PlayerScore += GetNearbyStencilsByTag(rule.TargetTag, stencil).Count * rule.RewardScore;
             }
         }
     }
 
-    List<StencilScobj> GetNearbyStencilsByTag(StencilTag tag)
+    List<StencilScobj> GetNearbyStencilsByTag(StencilTag tag, StencilObj baseStencil)
     {
         List<StencilScobj> results = new List<StencilScobj>();
-        foreach (StencilScobj stencil in GetNearbyStencils())
+        System.Collections.IList nearbyStencils = GetNearbyStencils(new Vector2(baseStencil.x, baseStencil.y));
+        for (int i = 0; i < nearbyStencils.Count; i++)
         {
+            StencilScobj stencil = (StencilScobj)nearbyStencils[i];
             if (stencil.tags.CompareTo(tag) == 1)
             {
                 results.Add(stencil);
@@ -33,11 +39,13 @@ public class ScoringMeowster : MonoBehaviour
         return results;
     }
 
-    List<StencilScobj> GetNearbyStencilsByName(string name)
+    List<StencilScobj> GetNearbyStencilsByName(string name, StencilObj baseStencil)
     {
         List<StencilScobj> results = new List<StencilScobj>();
-        foreach (StencilScobj stencil in GetNearbyStencils())
+        System.Collections.IList nearbyStencils = GetNearbyStencils(new Vector2(baseStencil.x, baseStencil.y));
+        for (int i = 0; i < nearbyStencils.Count; i++)
         {
+            StencilScobj stencil = (StencilScobj)nearbyStencils[i];
             if (stencil.name == name)
             {
                 results.Add(stencil);
@@ -49,6 +57,7 @@ public class ScoringMeowster : MonoBehaviour
 
     List<StencilObj> GetNearbyStencils(Vector2 pos)
     {
-
+        throw new NotImplementedException();
+        // return StencilManager.Instance.GetNearbyStencils(pos);
     }
 }
